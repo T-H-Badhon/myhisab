c7e92f30ca4e56752b75afcf1db320ea817ca5b2
T-H-Badhon <thbadhons@gmail.com>
2024-07-09 21:17:23 +0600

    gdfgh

--- src/components/home/addFunction.ts
+++ src/components/home/addFunction.ts
@@ -2,9 +2,12 @@
 
 import Transaction from "@/models/Transaction";
 import connectMongo from "@/utils/connectMongo";
+import { revalidatePath } from "next/cache";
 
 export const addFunction = async (reason: string, amount: number) => {
   await connectMongo();
 
   await Transaction.create({ reason: reason, amount: amount });
+
+  revalidatePath("/all");
 };
